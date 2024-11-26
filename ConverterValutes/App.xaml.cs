7e8e6067f6788398194c5fe670b4dd8a58077694
mihail B <beltyukov-mihail@mail.ru>
2024-11-26 13:44:52 +0500

    Добавьте файлы проекта.

---
+++ ConverterValutes/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ConverterValutes
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ ConverterValutes/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ConverterValutes
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
